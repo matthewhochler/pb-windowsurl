8440c4bcb78764e6f18bb156a23217b4a7262992
matthewhochler <matt@7parkdata.com>
2015-07-06 14:13:25 -0400

    Added tags

--- pbwindowsurl/export.py
+++ pbwindowsurl/export.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 
-import platform
+import os
 import re
 import string
 
 from .pinboard import Pinboard
+
+SAFE_CHARACTERS = (string.ascii_letters + string.digits + "-_.() ")
 
 
 def export(auth_token, path=None):
@@ -13,27 +15,24 @@ def export(auth_token, path=None):
     bookmarks = pb.bookmarks()
 
     for bookmark in bookmarks:
-        filename = re.sub(r'[\s\n\r]+', " ", bookmark['description'])
-        filename = filter(
-            lambda x: x in (string.ascii_letters + string.digits + "-_.() "),
-            filename,
-        )
-        filename = filename[:200]
+        filename = bookmark['description']
+        filename = re.sub(r'[\s\n\r]+', " ", filename)
+        filename = filter(lambda x: x in SAFE_CHARACTERS, filename)
 
         if not filename:
             continue
 
+        tags = bookmark['tags']
+        tags = filter(lambda x: x in SAFE_CHARACTERS, tags)
+        tags = ' [' + tags + ']'
+
+        filename = filename[:210 - len(tags)]
+        filename += tags
+        filename = filename.strip()
         filename += ".url"
 
         if path:
-            if platform.system() == "Windows":
-                if not path.endswith("\\"):
-                    path += "\\"
-            else:
-                if not path.endswith("/"):
-                    path += "/"
-
-            filename = path + filename
+            os.chdir(path)
 
         with open(filename, 'wb') as f:
             f.write("[InternetShortcut]\r\nURL=" + bookmark['href'])
